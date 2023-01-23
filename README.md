c475eac1138380584aed7bb2cd72bb3cca4ac869
AlphaQuinn <snowboarderquinn@gmail.com>
2023-01-23 13:47:30 -0500

    having difficulties with the saving function

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 3rd-party-apis2
+This is a single day planner which designates time stamps as present.past, or future based on colors.
+
+# Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
--- script.js
+++ script.js
@@ -1,24 +1,74 @@
 $(document).ready(function() {
-    let nowMoment=moment().format("MMMM Do YYYY");
+    let NowMoment=moment().format("MMMM Do YYYY");
     let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = nowMoment;
-    let currentHour = moment ().format("HH");
+    displayDate.innerHTML = NowMoment;
+    let currentHour = moment().format("HH");
 
     $("#clearfieldsBtn").click(function(event){
-        event.preventDefault;
+       event.preventDefault
         $("textarea").val("");
-        localStorage.clear(;)
+        localStorage.clear();
+    });
+
+    $(".time-div").each(function (){
+        var timeDiv = $(this).attr("id").split("-")[1];
+
+        if (currentHour == timeDiv) {
+            $(this).addClass("present");
+            $(this).children(".description").addClass(white-text)
+
+        } else if (currentHour<timeDiv) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } else if (currentHour>timeDiv){
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+
+
+    });
+    $(".saveBtn").click(function(event){
+        event.preventDefault();
+        var value = $(this).siblings(".time-block").val();
+        var time = $(this).parent().attr("id").split("-")[1];
+        localStorage.setItem(time,value);
     });
 
 
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-09 .time-block").val(localStorage.getItem("11"));
+    $("#hour-09 .time-block").val(localStorage.getItem("12"));
+    $("#hour-09 .time-block").val(localStorage.getItem("13"));
+    $("#hour-09 .time-block").val(localStorage.getItem("14"));
+    $("#hour-09 .time-block").val(localStorage.getItem("15"));
+    $("#hour-09 .time-block").val(localStorage.getItem("16"));
+    $("#hour-09 .time-block").val(localStorage.getItem("17"));
+});
 
-    
 
 
+$(document).ready(function(){var t=moment().format("MMMM Do YYYY");
+let e=document.getElementById("currentDay");
+e.innerHTML=t;var a=moment().format("HH");$("#clearFieldsBtn").click(function(t){t.preventDefault,
+    $("textarea").val(""),
+    localStorage.clear()}),
+    $(".time-div").each(function()
+    {var t=$(this).attr("id").split("-")[1];
+    a==t?($(this).addClass("present"),
+    $(this).children(".description").addClass("white-text")):a<t?($(this).removeClass("present"),
+    $(this).addClass("future")):t<a&&($(this).removeClass("future"),
+    $(this).addClass("past"))}),$(".saveBtn").click(function(t)
+    {t.preventDefault();var e=$(this).siblings(".time-block").val(),
+    t=$(this).parent().attr("id").split("-")[1];localStorage.setItem(t,e)},
+    $("#hour-09 .time-block").val(localStorage.getItem("09")),
+    $("#hour-10 .time-block").val(localStorage.getItem("10")),
+    $("#hour-11 .time-block").val(localStorage.getItem("11")),
+    $("#hour-12 .time-block").val(localStorage.getItem("12")),
+    $("#hour-13 .time-block").val(localStorage.getItem("13")),
+    $("#hour-14 .time-block").val(localStorage.getItem("14")),
+    $("#hour-15 .time-block").val(localStorage.getItem("15")),
+    $("#hour-16 .time-block").val(localStorage.getItem("16")),
+    $("#hour-17 .time-block").val(localStorage.getItem("17"))});
 
 
-
-
-
-
-}
